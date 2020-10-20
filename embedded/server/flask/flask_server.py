67992e7bd188c58581d6613b0162338ae884592f
Mat Muller <matuzalemmuller@gmail.com>
2020-10-20 17:32:58 -0300

    Fix dashboard UI and add email notification
    * Add email notification if temperature value is out of range
    * Remove mosquitto broker from docker-compose
    * Start implementation of environment variables in docker-compose file

--- embedded/server/flask/flask_server.py
+++ embedded/server/flask/flask_server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from flask_mail import Mail, Message
 
@@ -6,20 +7,21 @@ mail= Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'projetodetisa@gmail.com'
-app.config['MAIL_PASSWORD'] = 'tisa012345678'
+app.config['MAIL_USERNAME'] = os.environ.get('APP_EMAIL_ADDRESS')
+app.config['MAIL_PASSWORD'] = os.environ.get('APP_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/notificar")
 def index():
-  email = request.args.get('email')
+  email = str(request.args.get('email'))
+  temperatura = str(request.args.get('temperatura'))
   msg = Message('NOTIFICAÇÃO - Alteração de Temperatura', sender = 'projetodetisa@gmail.com', recipients = [email])
-  msg.body = "A temperatura atual é de " + str(request.args.get('temperatura'))
+  msg.body = "A temperatura atual é de " + temperatura
   mail.send(msg)
+  print("\n\n\n\n\n" + email + "   " + temperatura)
   return "Sent"
 
 if __name__ == '__main__':
-  print("test")
-  app.run(debug=True, port=80, host='172.18.1.4')
+  app.run(debug=True, port=80, host='172.18.1.3')
